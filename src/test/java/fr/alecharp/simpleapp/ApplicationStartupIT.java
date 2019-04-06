4aa8b2ad9418d9062ce3470518f9451bc3848f41
Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
2019-04-06 16:41:12 +0200

    Fix smoke test to new version of SpringBoot

--- src/test/java/fr/alecharp/simpleapp/ApplicationStartupIT.java
+++ src/test/java/fr/alecharp/simpleapp/ApplicationStartupIT.java
@@ -24,16 +24,14 @@ package fr.alecharp.simpleapp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * @author Adrien Lecharpentier
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(Server.class)
-@WebIntegrationTest(randomPort = true)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApplicationStartupIT {
     @Test
     public void shouldBeUpAndRunning() {
